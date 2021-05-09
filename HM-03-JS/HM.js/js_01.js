59fdb6e16e69f93452b3abca04e0841980d6c44d
LvCanfa <83910567+LvCanfa@users.noreply.github.com>
2021-05-09 23:43:30 +0800

    Add files via upload

---
+++ HM-03-JS/HM.js/js_01.js
@@ -0,0 +1,31 @@
+  // alert('什么时候去酒吧？');
+  /* // 获取时间
+  function countDown(time){
+    var nowTime = +new Date();
+    var inputTime = +new Date(time);
+    var times =( inputTime - nowTime)/1000; //times是剩余时间的总秒数
+    var d = parseInt(times /60 /60 /24); //时
+    d = d <10? '0'+d : d;
+    var h = parseInt(times /60 /60 %24); //时
+    h = h <10? '0'+h : h;
+    var m=parseInt(times /60 %60);
+    m=m<10 ? '0'+ m : m;
+    var s=parseInt(times %60);
+    s=s<10 ? '0'+ s : s;
+    return d+'天'+h +'时'+m +'分'+s+'秒';
+  }
+  console.log(countDown(2022-3-11));
+  var date=new Date();
+  console.log(date); */
+  /* //去重函数 
+  function unique(arr){
+    var newArr=[];
+    for(var i=0;i<arr.length;i++){
+      if (newArr.indexOf(arr[i])===-1){
+        newArr.push(arr[i]);
+      }
+    }
+    return newArr;
+  }
+  var demo = unique(['a','a','a','a','a','a','a12','a12','aaa'])
+  console.log(demo); */
